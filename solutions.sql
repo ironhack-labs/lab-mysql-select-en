5377ed257955949bb37ea604d724a8d5f9d96ca6
Salvacat <salvador.vento.a@gmail.com>
2024-09-13 16:50:04 +0200

    task finished

---
+++ solutions.sql
@@ -0,0 +1,60 @@
+
+
+-- Solution Challange 1
+
+SELECT authors.au_id AS "AUTHOR ID", authors.au_lname AS "LAST NAME", authors.au_fname AS "FIRST NAME",
+titles.title AS "TITLE", publishers.pub_name AS "PUBLISHER"
+
+FROM authors, titles, publishers, titleauthor
+
+WHERE authors.au_id == titleauthor.au_id AND
+titleauthor.title_id == titles.title_id AND
+titles.pub_id == publishers.pub_id
+
+
+
+-- Solution Challange 2
+
+SELECT authors.au_id AS "AUTHOR ID", authors.au_lname AS "LAST NAME", authors.au_fname AS "FIRST NAME",
+ publishers.pub_name AS "PUBLISHER", count(titles.title) AS "TITLE COUNT"
+
+FROM authors, titles, publishers, titleauthor
+
+WHERE authors.au_id == titleauthor.au_id AND
+titleauthor.title_id == titles.title_id AND
+titles.pub_id == publishers.pub_id
+
+GROUP BY authors.au_id, publishers.pub_id
+ORDER BY authors.au_fname ASC
+;
+
+
+-- Solution Challange 3
+
+SELECT authors.au_id AS "AUTHOR ID", authors.au_lname AS "LAST NAME", authors.au_fname AS "FIRST NAME",
+ sum(sales.qty) AS "TOTAL"
+
+FROM authors, titles, sales, titleauthor
+
+WHERE authors.au_id == titleauthor.au_id AND
+titleauthor.title_id == titles.title_id AND
+titles.title_id == sales.title_id
+GROUP BY authors.au_id
+ORDER BY "TOTAL" DESC
+LIMIT 3
+;
+
+
+-- Solution Challange 4
+SELECT authors.au_id AS "AUTHOR ID", 
+       authors.au_lname AS "LAST NAME", 
+       authors.au_fname AS "FIRST NAME", 
+       COALESCE(SUM(sales.qty), 0) AS "TOTAL"
+FROM authors
+LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+LEFT JOIN titles ON titleauthor.title_id = titles.title_id
+LEFT JOIN sales ON titles.title_id = sales.title_id
+GROUP BY authors.au_id
+ORDER BY "TOTAL" DESC;
+
+
