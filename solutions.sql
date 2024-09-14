05a5b4376bfb687c1804b50909a7d6c7399b637a
Diego_Alonso <dalonso.apg@gmail.com>
2024-09-15 00:32:27 +0200

    Lab 2 SQL Finished

--- solutions.sql
+++ solutions.sql
@@ -1,31 +1,31 @@
 -- Solutions to SQL Labs2
 -- Solution to Challenge 1
 
-SELECT authors.au_id AS 'AUTHOR ID', 
+SELECT 
+authors.au_id AS 'AUTHOR ID', 
 authors.au_lname AS 'LAST NAME', 
 authors.au_fname AS 'FIRST NAME', 
 titles.title AS 'TITLE', 
 publishers.pub_name AS 'PUBLISHER'
 FROM authors
-LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
-LEFT JOIN titles ON titleauthor.title_id = titles.title_id
-LEFT JOIN publishers ON titles.pub_id = publishers.pub_id;
+JOIN titleauthor ON authors.au_id = titleauthor.au_id
+JOIN titles ON titles.title_id = titleauthor.title_id
+JOIN publishers ON titles.pub_id = publishers.pub_id
 
 -- Solution to Challenge 2
 
-SELECT 
-authors.au_id as 'AUTHOR ID',
-authors.au_lname as 'LAST NAME',
-authors.au_fname as 'FIRST NAME', 
-titles.title as 'TITLE',
-publishers.pub_name as 'PUBLISHER',
-COUNT(titles.title) as 'TITLE COUNT'
+SELECT
+authors.au_id AS 'AUTHOR ID', 
+authors.au_lname AS 'LAST NAME', 
+authors.au_fname AS 'FIRST NAME', 
+publishers.pub_name AS 'PUBLISHER', 
+COUNT(titles.title_id) AS 'TITLE COUNT'
 FROM authors
-LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
-LEFT JOIN titles ON titleauthor.title_id = titles.title_id
-LEFT JOIN publishers ON titles.pub_id = publishers.pub_id
-GROUP BY
-authors.au_id, authors.au_lname, authors.au_fname, publishers.pub_name
+JOIN titleauthor ON authors.au_id = titleauthor.au_id
+JOIN titles ON titles.title_id = titleauthor.title_id
+JOIN publishers ON titles.pub_id = publishers.pub_id
+GROUP BY publishers.pub_name, authors.au_id
+ORDER BY 'TITLE COUNT' DESC
 
 -- Solution to Challenge 3
 
@@ -35,9 +35,9 @@ authors.au_lname AS 'LAST NAME',
 authors.au_fname AS 'FIRST NAME',
 SUM (sales.qty)  AS 'TOTAL'
 FROM authors
-LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
-LEFT JOIN titles ON titleauthor.title_id = titles.title_id
-LEFT JOIN sales ON titles.title_id = sales.title_id
+JOIN titleauthor ON authors.au_id = titleauthor.au_id
+JOIN titles ON titleauthor.title_id = titles.title_id
+JOIN sales ON titles.title_id = sales.title_id
 GROUP BY 
 authors.au_id, authors.au_lname, authors.au_fname
 ORDER BY 
