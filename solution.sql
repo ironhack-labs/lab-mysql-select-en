85e219c10c4d594b4a7ce32a83d58a2e2385b997
Your Name <paulmueller1983@gmail.com>
2024-11-23 12:35:56 +0100

    Rework

--- solution.sql
+++ solution.sql
@@ -1,59 +1,56 @@
 # querry 1
 
 SELECT 
-    authors.au_id AS "Author ID", 
-    authors.au_lname AS "Last name", 
+    authors.au_id AS "Author ID",
+    authors.au_lname AS "Last name",
     authors.au_fname AS "First name",
     titles.title AS "Title",
     publishers.pub_name as "Publisher"
-
-FROM authors, titleauthor, titles, publishers
-
-WHERE
-	authors.au_id == titleauthor.au_id AND
-	titleauthor.title_id == titles.title_id AND
-	titles.pub_id == publishers.pub_id
+FROM 
+    authors
+    JOIN titleauthor ON authors.au_id = titleauthor.au_id
+    JOIN titles ON titleauthor.title_id = titles.title_id
+    JOIN publishers ON titles.pub_id = publishers.pub_id;
 
 
 # querry 2
 
 SELECT
-	authors.au_id AS "Author ID", 
-	authors.au_lname AS "Last name", 
-	authors.au_fname AS "First name",
-	count(titles.title) AS "Title__count",
-	publishers.pub_name as "Publisher"
-
-FROM authors, titleauthor, titles, publishers
-
-WHERE
-	authors.au_id == titleauthor.au_id AND
-	titleauthor.title_id == titles.title_id AND
-	titles.pub_id == publishers.pub_id
+    authors.au_id AS "Author ID",
+    authors.au_lname AS "Last name",
+    authors.au_fname AS "First name",
+    COUNT(titles.title_id) AS "Title Count",
+    publishers.pub_name as "Publisher"
+FROM 
+    authors
+    LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+    LEFT JOIN titles ON titleauthor.title_id = titles.title_id
+    LEFT JOIN publishers ON titles.pub_id = publishers.pub_id
 GROUP BY 
-	authors.au_id, 
-	authors.au_lname, 
-	authors.au_fname, 
-	publishers.pub_name	
+    authors.au_id, 
+    authors.au_lname, 
+    authors.au_fname, 
+    publishers.pub_name
+ORDER BY
+    "Title Count" DESC;
 
 # query 3
 
 SELECT
-    authors.au_id AS "Author ID", 
-    authors.au_lname AS "Last Name", 
+    authors.au_id AS "Author ID",
+    authors.au_lname AS "Last Name",
     authors.au_fname AS "First Name",
     SUM(titles.ytd_sales) AS "Total"
 FROM
-    authors, titleauthor, titles
-WHERE
-    authors.au_id = titleauthor.au_id AND
-    titleauthor.title_id = titles.title_id
+    authors
+    LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+    LEFT JOIN titles ON titleauthor.title_id = titles.title_id
 GROUP BY 
     authors.au_id, 
     authors.au_lname, 
     authors.au_fname
 ORDER BY 
-    SUM(titles.ytd_sales) DESC
+    "Total" DESC
 LIMIT 3;
 
 # query 4
@@ -64,27 +61,12 @@ SELECT
     authors.au_fname AS "First Name",
     COALESCE(SUM(titles.ytd_sales), 0) AS "Total"
 FROM
-    authors, titleauthor, titles
-WHERE
-    authors.au_id = titleauthor.au_id AND
-    titleauthor.title_id = titles.title_id
+    authors
+    LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+    LEFT JOIN titles ON titleauthor.title_id = titles.title_id
 GROUP BY 
     authors.au_id, 
     authors.au_lname, 
     authors.au_fname
-
-UNION
-
-SELECT
-    authors.au_id AS "Author ID",
-    authors.au_lname AS "Last Name",
-    authors.au_fname AS "First Name",
-    0 AS "Total"
-FROM
-    authors
-WHERE 
-    authors.au_id NOT IN (xa
-        SELECT DISTINCT au_id FROM titleauthor
-    )
 ORDER BY 
     "Total" DESC;
