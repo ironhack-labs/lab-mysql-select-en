3726c63481c5d54d13243381481808b158c23409
ECG01 <edcruzgarcia14@gmail.com>
2024-12-06 17:06:01 -0400

    lab_sql_ecg

--- solution.sql
+++ solution.sql
@@ -33,11 +33,11 @@ SELECT
     a.au_id AS AUTHOR_ID,
     a.au_lname AS LAST_NAME,
     a.au_fname AS FIRST_NAME,
-    SUM(t.ytd_sales) AS TITLE_SALES
+    SUM(case when t.title_id = s.title_id then s.qty else 0 end) AS TITLE_SALES
 FROM authors a
 JOIN titleauthor at ON a.au_id = at.au_id
 JOIN titles t ON at.title_id = t.title_id
-JOIN sales s ON t.title_id = s.title_id
+LEFT JOIN sales s ON t.title_id = s.title_id
 GROUP BY a.au_id, a.au_lname, a.au_fname
 ORDER BY TITLE_SALES DESC
 LIMIT 3;
