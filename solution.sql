5883b3d1be1f9364332df536979bcca6b40cebc3
ECG01 <edcruzgarcia14@gmail.com>
2024-12-06 17:10:06 -0400

    lab_sql_select_ecg

--- solution.sql
+++ solution.sql
@@ -43,14 +43,13 @@ ORDER BY TITLE_SALES DESC
 LIMIT 3;
 
  /*    CHALLENGE 4      */
- 
-SELECT 
+ SELECT 
     a.au_id AS AUTHOR_ID,
     a.au_lname AS LAST_NAME,
     a.au_fname AS FIRST_NAME,
-    COUNT(t.title_id) AS TITLE_COUNT
+    IFNULL(SUM(t.ytd_sales), 0) AS TOTAL_SALES
 FROM authors a
 LEFT JOIN titleauthor at ON a.au_id = at.au_id
 LEFT JOIN titles t ON at.title_id = t.title_id
 GROUP BY a.au_id, a.au_lname, a.au_fname
-ORDER BY TITLE_COUNT DESC;
+ORDER BY TOTAL_SALES DESC;
