bec48855c440a2bcce2de56fb76d1935b51f9501
javierdastas <javier.dastas@outlook.com>
2024-12-16 15:54:11 -0400

    Create the solution.sql for the lab-mysql-select-en challenge.
    Create the solution.sql for the lab-mysql-select-en challenge. Week 3, Day 1, Challenge 2.

--- solution.sql
+++ solution.sql
@@ -20,20 +20,22 @@ order by  a.au_id desc,  count(t.title) desc
 
 --- Challenge 3
 select a.au_id 'AUTHOR ID', a.au_lname 'LAST NAME', a.au_fname 'FIRST NAME',
-        count(t.title) 'TOTAL'
+         sum(sales.qty) AS 'TOTAL'
 from authors a
 join titleauthor ta on a.au_id = ta.au_id
 join titles t on ta.title_id = t.title_id
+join sales on t.title_id = sales.title_id
 group by a.au_id, a.au_lname, a.au_fname
-order by count(t.title) desc
+order by sum(sales.qty) DESC
 limit 3;
 
 --- Challenge 4
 select a.au_id 'AUTHOR ID', a.au_lname 'LAST NAME', a.au_fname 'FIRST NAME',
-        count(t.title) 'TOTAL'
+         ifnull(sum(sales.qty), 0) AS 'TOTAL'
 from authors a
 left join titleauthor ta on a.au_id = ta.au_id
 left join titles t on ta.title_id = t.title_id
+left join sales on t.title_id = sales.title_id    
 group by a.au_id, a.au_lname, a.au_fname
-order by count(t.title) desc
+order by ifnull(sum(sales.qty), 0) desc
 limit 23;
